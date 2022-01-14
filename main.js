eb44e36218597d4a48a4a5ac9d2569ca7ef5d129
Gabriel Halus <gabrielhalus@gmail.com>
2022-01-14 10:05:22 +0100

    No Scroll On Modal

--- main.js
+++ main.js
@@ -12,17 +12,20 @@ var span = document.getElementsByClassName("close") [0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
+    document.body.style.overflow = "hidden"
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    document.body.style.overflow = "auto"
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if(event.target == modal) {
         modal.style.display = "none";
+        document.body.style.overflow = "auto"
     } 
 } 
 
@@ -40,17 +43,21 @@ var span = document.getElementsByClassName("close") [1];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
+    document.body.style.overflow = "hidden"
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    document.body.style.overflow = "auto"
+
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if(event.target == modal) {
         modal.style.display = "none";
+        document.body.style.overflow = "auto"
     } 
 } 
 
@@ -68,16 +75,19 @@ var span = document.getElementsByClassName("close") [2];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
+    document.body.style.overflow = "hidden"
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    document.body.style.overflow = "auto"
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if(event.target == modal) {
         modal.style.display = "none";
+        document.body.style.overflow = "auto"
     } 
 } 
