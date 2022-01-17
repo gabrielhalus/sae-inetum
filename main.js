f7b96f0154d642185204d48635a5ab633eb6340a
Gabriel Halus <gabrielhalus@gmail.com>
2022-01-17 15:03:33 +0100

    TG Samuel

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 // SECTOR
 
 // Get the modal
-var modal = document.getElementById("modal-sector");
+var sector_modal = document.getElementById("modal-sector");
 
 // Get the button that opens the modal
-var btn = document.getElementById("btn-sector");
+var sector_btn = document.getElementById("btn-sector");
 
 //Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close") [0];
+var sector_span = document.getElementsByClassName("close") [0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+sector_btn.onclick = function() {
+    sector_modal.style.display = "block";
     document.body.style.overflow = "hidden"
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+sector_span.onclick = function() {
+    sector_modal.style.display = "none";
     document.body.style.overflow = "auto"
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if(event.target == modal) {
-        modal.style.display = "none";
+    if(event.target == sector_modal) {
+        sector_modal.style.display = "none";
         document.body.style.overflow = "auto"
     } 
 } 
@@ -32,62 +32,62 @@ window.onclick = function(event) {
 // TRANSITION
 
 // Get the modal
-var modal = document.getElementById("modal-transition");
+var transition_modal = document.getElementById("modal-transition");
 
 // Get the button that opens the modal
-var btn = document.getElementById("btn-transition");
+var transition_btn = document.getElementById("btn-transition");
 
 //Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close") [1];
+var transition_span = document.getElementsByClassName("close") [1];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+transition_btn.onclick = function() {
+    transition_modal.style.display = "block";
     document.body.style.overflow = "hidden"
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+transition_span.onclick = function() {
+    transition_modal.style.display = "none";
     document.body.style.overflow = "auto"
 
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if(event.target == modal) {
-        modal.style.display = "none";
+    if(event.target == transition_modal) {
+        transition_modal.style.display = "none";
         document.body.style.overflow = "auto"
     } 
 } 
-
+ 
 // CLIENTS
 
 // Get the modal
-var modal = document.getElementById("modal-clients");
+var clients_modal = document.getElementById("modal-clients");
 
 // Get the button that opens the modal
-var btn = document.getElementById("btn-clients");
+var clients_btn = document.getElementById("btn-clients");
 
 //Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close") [2];
+var clients_span = document.getElementsByClassName("close") [2];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+clients_btn.onclick = function() {
+    clients_modal.style.display = "block";
     document.body.style.overflow = "hidden"
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+clients_span.onclick = function() {
+    clients_modal.style.display = "none";
     document.body.style.overflow = "auto"
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if(event.target == modal) {
-        modal.style.display = "none";
+    if(event.target == clients_modal) {
+        clients_modal.style.display = "none";
         document.body.style.overflow = "auto"
     } 
 } 
